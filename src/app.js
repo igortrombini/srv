fe1d2e3ff6e3e35278b35a1c74b939d3bda6f58a
igortrombini <igorts20@gmail.com>
2023-11-13 20:09:40 -0300

    add:cors

--- src/app.js
+++ src/app.js
@@ -1,26 +1,30 @@
 import 'dotenv/config';
-import express from "express";
-import cors from "cors";
-import path from 'node:path';
-import { router } from "./routes";
-
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { router } from './routes';
 
 const app = express();
-//SERVE IMAGENS STATICAS
+
+// SERVE IMAGENS STATICAS
 app.use('/uploads', express.static(path.resolve(__dirname, '../', 'uploads')));
 app.use(express.json());
 
+// Enable CORS
+app.use(cors());
+
+// CORS headers
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  app.use(cors());
+  res.header('Access-Control-Allow-Origin', 'https://front-imobi.onrender.com');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
 
 app.use(router);
 
 app.listen(8000, () => {
-  console.log("Server is runing 8000");
-})
+  console.log('Server is running on port 8000');
+});
+
 console.log(process.env.DATABASE_URL);
