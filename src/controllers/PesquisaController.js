8d6a85fdd5962c08b0bc4b1c8ebf8fc9281ad035
igortrombini <igorts20@gmail.com>
2023-11-27 16:44:04 -0300

    fix:PesquisaController

--- src/controllers/PesquisaController.js
+++ src/controllers/PesquisaController.js
@@ -1,14 +1,19 @@
 const { PrismaClient } = require("@prisma/client");
+const unorm = require("unorm");
+
 const prisma = new PrismaClient();
 
 async function pesquisarImoveis(request, response) {
   try {
     const { cidade } = request.query;
 
+    // Normaliza a entrada da cidade para ser insensível a acentos
+    const cidadeNormalizada = unorm.nfkd(cidade).replace(/[\u0300-\u036f]/g, "");
+
     const resultadosDaBusca = await prisma.imobi.findMany({
       where: {
         cidade: {
-          contains: cidade,
+          contains: cidadeNormalizada,
         },
       },
     });
