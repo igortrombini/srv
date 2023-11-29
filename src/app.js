1de16fa27e216123b053c025f523a41866a841d0
igortrombini <igorts20@gmail.com>
2023-11-28 22:55:21 -0300

    add:CORS

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(cors());
 // CORS headers
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://front-imobi.onrender.com');
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  //res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
